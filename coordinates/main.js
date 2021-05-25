b1ce1a57cbaa8a1c422352e17a50134a9df803e6
sunysty <seokjiseon@gmail.com>
2021-05-25 12:43:13 +0900

    성능개선 : position -> transition

--- coordinates/main.js
+++ coordinates/main.js
@@ -5,19 +5,23 @@ const horozontal = document.querySelector('.horozontal');
 const target = document.querySelector('.target');
 const tag = document.querySelector('.tag');
 
-document.addEventListener('mousemove', event => {
-    const x = event.clientX
-    const y = event.clientY
+
+addEventListener('load', () => {
+    const targetRect = target.getBoundingClientRect();
+    const targetHalfWidth = targetRect.width / 2;
+    const targetHalfHeight = targetRect.height / 2;
+
+    document.addEventListener('mousemove', event => {
+        const x = event.clientX
+        const y = event.clientY
+        
+        //style의 left, top값을 동적으로 움직이게 만듬
+        vertical.style.transform = `translateX(${x}px)`;
+        horozontal.style.transform = `translateY(${y}px)`;
+        target.style.transform = `translate(${x - targetHalfWidth}px,${y - targetHalfHeight}px)`;
+        tag.innerHTML = `${x}px, ${y}px`;
+        tag.style.transform = `translate(${x+20}px,${y+20}px)`;
+    })
+});
 
 
-    //style의 left, top값을 동적으로 움직이게 만듬
-    vertical.style.left = `${x}px`;
-    horozontal.style.top = `${y}px`;
-    target.style.left = `${x}px`;
-    target.style.top = `${y}px`;
-    tag.style.left = `${x}px`;
-    tag.style.top = `${y}px`;
-
-    //inner html의 값을 변경해주어야 함
-    tag.innerHTML = `${x}px, ${y}px`;
-})
