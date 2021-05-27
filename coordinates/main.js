44de4ce56922e5c2f7a3baa720c0feb207c695b2
sunysty <seokjiseon@gmail.com>
2021-05-27 22:26:38 +0900

    first commit

--- coordinates/main.js
+++ coordinates/main.js
@@ -15,12 +15,11 @@ addEventListener('load', () => {
         const x = event.clientX
         const y = event.clientY
         
-        //style의 left, top값을 동적으로 움직이게 만듬
         vertical.style.transform = `translateX(${x}px)`;
         horozontal.style.transform = `translateY(${y}px)`;
-        target.style.transform = `translate(${x - targetHalfWidth}px,${y - targetHalfHeight}px)`;
+        target.style.transform = `translate(${x - targetHalfWidth}px, ${y - targetHalfHeight}px)`;
         tag.innerHTML = `${x}px, ${y}px`;
-        tag.style.transform = `translate(${x+20}px,${y+20}px)`;
+        tag.style.transform = `translate(${x + 20}px,${y + 20}px)`;
     })
 });
 
